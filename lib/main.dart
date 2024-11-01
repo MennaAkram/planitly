258700be0ec8b9a88844bb2c171a71aca89fc14e
mennaakram <mennaakram976@gmail.com>
2024-11-01 16:22:48 +0200

    add files in gitignore,theme in main and rename add button file

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:planitly/shared/widgets/add_button.dart';
-import 'package:planitly/shared/widgets/app_bar.dart';
-import 'package:planitly/shared/widgets/button.dart';
-import 'package:planitly/shared/widgets/calendar.dart';
-import 'package:planitly/shared/widgets/drop_down_list.dart';
-import 'package:planitly/shared/widgets/text_field.dart';
-import 'package:planitly/shared/widgets/title.dart';
+import 'design_system/app_colors.dart';
+import 'design_system/app_text.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,174 +9,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-        useMaterial3: true,
+      theme: Theme.of(context).copyWith(
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+          primary: AppColorsTheme.light().primary,
+          secondary: AppColorsTheme.light().secondary,
+          surface: AppColorsTheme.light().background,
+          error: AppColorsTheme.light().red,
+        ),
+        extensions: [
+          AppColorsTheme.light(),
+          AppTextsTheme.main(),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      // _counter++;
-    });
-  }
-
-  List<String> items = ["music", "games", "tasks"];
-
-  //================= you can use this to show the calendar ==========================
-  bool showCalendar = true;
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      body: showCalendar
-          ? const Padding(
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: CalendarWidget(),
-            )
-          : Container(
-              color: Colors.white,
-              // Center is a layout widget. It takes a single child and positions it
-              // in the middle of the parent.
-              child: Column(
-                // Column is also a layout widget. It takes a list of children and
-                // arranges them vertically. By default, it sizes itself to fit its
-                // children horizontally, and tries to be as tall as its parent.
-                //
-                // Column has various properties to control how it sizes itself and
-                // how it positions its children. Here we use mainAxisAlignment to
-                // center the children vertically; the main axis here is the vertical
-                // axis because Columns are vertical (the cross axis would be
-                // horizontal).
-                //
-                // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-                // action in the IDE, or press "p" in the console), to see the
-                // wireframe for each widget.
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  const SizedBox(height: 15),
-                  const CustomAppBar(title: "Subject Name"),
-                  const SizedBox(height: 10),
-                  const CustomTitle(
-                    title: "custom Text Field",
-                    seeAll: false,
-                  ),
-                  const SizedBox(height: 10),
-                  CustomTextField(
-                      labelText: "Email", controller: TextEditingController()),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  CustomTextField(
-                    labelText: "Password",
-                    controller: TextEditingController(),
-                    isPassword: true,
-                  ),
-                  const SizedBox(height: 10),
-                  const CustomTitle(
-                    title: "custom Drop Down List",
-                    seeAll: false,
-                  ),
-                  const SizedBox(height: 10),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 30),
-                    child: const DropDownList(
-                      hintText: "habits",
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const CustomTitle(
-                    title: "custom Button",
-                  ),
-                  const SizedBox(height: 10),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  CustomButton(
-                    text: "SignUp",
-                    onPressed: () {},
-                    outlined: false,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  CustomButton(
-                    text: "SignUp",
-                    onPressed: () {},
-                    outlined: true,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  CustomButton(
-                    text: "add new income",
-                    onPressed: () {},
-                    outlined: true,
-                    addIcon: true,
-                    verticalPadding: 0,
-                    horizontalPadding: 0,
-                    width: 150,
-                  ),
-                ],
-              ),
-            ),
-      floatingActionButton: AddButton(onPressed: _incrementCounter),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
