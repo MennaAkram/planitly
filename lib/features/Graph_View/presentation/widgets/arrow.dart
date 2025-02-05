50fb870cdb3d1a1a5a214548eee8624ac5ae69bd
Youssef-Ahmed238999 <163328420+Youssef-Ahmed238999@users.noreply.github.com>
2025-02-05 18:44:42 +0200

    fix arrow widget

--- lib/features/Graph_View/presentation/widgets/arrow.dart
+++ lib/features/Graph_View/presentation/widgets/arrow.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:planitly/design_system/theme.dart';
 import 'package:planitly/features/Graph_View/presentation/cubit/graph_view_operations.dart';
 
@@ -17,15 +18,10 @@ class Arrow extends StatelessWidget {
               children: [
                 Transform.rotate(
                   angle: direction == Directions.right ? 0 : 3.14 * -180 / 180,
-                  child: ClipPath(
-                    clipper: _CustomClipper(),
-                    child: Container(
-                      width: 48,
-                      height: 23,
-                      decoration: BoxDecoration(
-                        gradient: Theme.of(context).appColors.gradientLR,
-                      ),
-                    ),
+                  child: SvgPicture.asset(
+                    'assets/icons/arrow.svg',
+                    width: 48,
+                    height: 23,
                   ),
                 ),
                 Text(relation,
@@ -42,15 +38,10 @@ class Arrow extends StatelessWidget {
                   angle: direction == Directions.down
                       ? 3.14 * 90 / 180
                       : 3.14 * -90 / 180,
-                  child: ClipPath(
-                    clipper: _CustomClipper(),
-                    child: Container(
-                      width: 48,
-                      height: 23,
-                      decoration: BoxDecoration(
-                        gradient: Theme.of(context).appColors.gradientLR,
-                      ),
-                    ),
+                  child: SvgPicture.asset(
+                    'assets/icons/arrow.svg',
+                    width: 48,
+                    height: 23,
                   ),
                 ),
                 Text(relation,
@@ -63,27 +54,3 @@ class Arrow extends StatelessWidget {
   }
 }
 
-class _CustomClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    path.moveTo(0, size.height * 0.4);
-    path.lineTo(size.width * 0.875, size.height * 0.4);
-    path.lineTo(size.width * 0.75, size.height * 0.18);
-    path.quadraticBezierTo(size.width * 0.65, 0, size.width * 0.75, 0);
-    path.lineTo(size.width * 0.95, size.height * 0.4);
-    path.quadraticBezierTo(
-        size.width, size.height * 0.5, size.width * 0.95, size.height * 0.6);
-    path.lineTo(size.width * 0.95, size.height * 0.6);
-    path.lineTo(size.width * 0.75, size.height);
-    path.quadraticBezierTo(
-        size.width * 0.65, size.height, size.width * 0.75, size.height * 0.82);
-    path.lineTo(size.width * 0.875, size.height * 0.6);
-    path.lineTo(0, size.height * 0.6);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
-}
