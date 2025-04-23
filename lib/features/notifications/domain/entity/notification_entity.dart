42c53afe582874f38abe7d8dd66646cd800793c3
Youssef Ahmed <163328420+Youssef-Ahmed238999@users.noreply.github.com>
2025-04-23 21:01:37 +0200

    add NotificationEntity class to manage notification data

---
+++ lib/features/notifications/domain/entity/notification_entity.dart
@@ -0,0 +1,11 @@
+class NotificationEntity {
+  final String id;
+  final String message;
+  final String created_at;
+
+  NotificationEntity({
+    required this.id,
+    required this.message,
+    required this.created_at,
+  });
+}
