48c2f92139a5eae05bfd0e0cfa52915c3a9fca71
Youssef Ahmed <163328420+Youssef-Ahmed238999@users.noreply.github.com>
2025-05-20 14:22:19 +0300

    add pages repository interface

---
+++ lib/features/my_pages/domain/repositories/pages_repo.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:planitly/features/my_pages/domain/entity/pages_info_entity.dart';
+import 'package:planitly/shared/networking/failures.dart';
+
+abstract class PagesRepository {
+  Future<Either<NetworkException, PagesInfoEntity>> getPages();
+}
