f8534777f7ccdd757bfc9b0267b4cad95f5183ae
Youssef Ahmed <163328420+Youssef-Ahmed238999@users.noreply.github.com>
2025-05-22 20:18:33 +0300

    add categories repo

---
+++ lib/features/categories/domain/repositories/categories_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:planitly/features/categories/domain/entity/categories_info_entity.dart';
+import 'package:planitly/shared/networking/failures.dart';
+
+abstract class CategoriesRepo {
+  Future<Either<NetworkException, CategoriesInfoEntity>> getCategories(
+      {int offset = 0});
+}
