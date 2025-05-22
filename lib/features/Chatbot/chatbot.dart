de75647dfab2ceca58ded9029dd62e31c6181d07
SandyEmad1 <sandyemad544@gmail.com>
2025-05-22 23:39:28 +0300

    fixing errors

--- lib/features/Chatbot/chatbot.dart
+++ lib/features/Chatbot/chatbot.dart
@@ -1,9 +1,7 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:planitly/design_system/theme.dart';
-import 'package:planitly/shared/assests.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:planitly/shared/assets.dart';
 
 class Chatbot extends StatefulWidget {
   const Chatbot({super.key});
@@ -13,158 +11,205 @@ class Chatbot extends StatefulWidget {
 }
 
 class _ChatbotState extends State<Chatbot> {
-  final ScrollController _scrollController = ScrollController();
-  TextEditingController massagecontroler = TextEditingController();
+     TextEditingController massagecontroler = TextEditingController();
   // ignore: prefer_final_fields
-  List<Map<String, String>> _messages = [];
+  List<Map<String, dynamic>> _conversationTurns = [];
   GlobalKey<FormState> stateform = GlobalKey();
 
-  void _sendMessage() {
-    String text = massagecontroler.text.trim();
+  ScrollController _scrollController = ScrollController(); 
+  bool _isTyping = false;
+void _sendMessage() {
+    final text = massagecontroler.text.trim();
     if (text.isNotEmpty) {
       setState(() {
-        _messages.add({"sender": "user", "text": text});
-        _messages.add({"sender": "bot", "text": _getBotResponse(text)});
+        _conversationTurns
+            .add({'user': text, 'bot': '...'});
+        massagecontroler.clear();
+        _scrollToBottom();
       });
-      massagecontroler.clear();
+
+      
+      Future.delayed(const Duration(milliseconds: 1000), () {
+        final botResponse = _getBotResponse(text);
+        setState(() {
+          if (_conversationTurns.isNotEmpty) {
+            _conversationTurns.last['bot'] =
+                botResponse; 
+            _scrollToBottom();
+          }
+        });
+      });
     }
   }
-
-  String _getBotResponse(String userMessage) {
+    
+ String _getBotResponse(String userMessage) {
     return "Hi how I can help you? ";
   }
 
+     void _scrollToBottom() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_scrollController.hasClients) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+        );
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              title: Wrap(children: [
-                SvgPicture.asset(Assests.chatbot),
-                Text(
-                  "ChatBot",
-                  style: Theme.of(context).appTexts.bodyLarge,
-                ),
-              ]),
-            ),
-            body: Column(
+    return  Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Wrap(children: [
+          Container(height: 24,
+          width: 24,
+          margin: EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(image: AssetImage(Assets.chatbot))),
+            
+          ),
+          Text(
+            "ChatBot",
+            style: Theme.of(context).appTexts.bodyLarge,
+          ),
+        ]),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 10,
+            child: Stack(
               children: [
-                Expanded(
-                    flex: 10,
-                    child: Stack(
-                      children: [
-                        Container(
-                          height: double.infinity,
-                          width: double.infinity,
-                          color: Theme.of(context).appColors.white100,
-                          child: FittedBox(
-                              fit: BoxFit.cover,
-                              child: SvgPicture.asset(Assests.background)),
-                        ),
-                        ListView.builder(
-                          controller: _scrollController,
-                          itemCount: _messages.length,
-                          itemBuilder: (context, index) {
-                            var message = _messages[index];
-                            return ListTile(
-                              title: Align(
-                                alignment: message['sender'] == 'user'
-                                    ? Alignment.centerRight
-                                    : Alignment.centerLeft,
-                                child: Container(
-                                  padding: EdgeInsets.symmetric(
-                                      vertical: 8, horizontal: 12),
-                                  decoration: BoxDecoration(
-                                    color: message['sender'] == 'user'
-                                        ?Theme.of(context).appColors.primary
-                                        : Theme.of(context).appColors.black16,
-                                    borderRadius: message['sender'] == 'user'
-                                        ? BorderRadius.only(
-                                            topLeft: Radius.circular(16),
-                                            topRight: Radius.circular(16),
-                                            bottomLeft: Radius.circular(16))
-                                        : BorderRadius.only(
-                                            topLeft: Radius.circular(16),
-                                            topRight: Radius.circular(16),
-                                            bottomRight: Radius.circular(16)),
-                                  ),
-                                  child: Text(
-                                    message['text']!,
-                                    style: TextStyle(
-                                      color: message['sender'] == 'user'
-                                          ? Theme.of(context).appColors.white100
-                                          : Theme.of(context).appColors.black87,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            );
-                          },
-                        )
-                      ],
-                    )),
+                Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  color: Theme.of(context).appColors.white100,
+                  child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: SvgPicture.asset(Assets.background)),
+                ),
+                ListView.builder(
+                  controller: _scrollController,
+                  itemCount: _conversationTurns.length + (_isTyping ? 1 : 0),
+                  itemBuilder: (context, index) {
+                    final turn = _conversationTurns[index];
+                    final userMessage = turn['user'];
+                    final botMessage = turn['bot'];
 
-                Expanded(
-                  flex: 1,
-                  child: SizedBox(
-                      height: 50,
-                      child: Row(
-                        children: [
-                          Expanded(
-                              flex: 12,
-                              child: Container(
-                                  height: 38,
-                                  width: double.maxFinite,
-                                  margin:
-                                       EdgeInsets.symmetric(vertical: 8),
-                                  padding:
-                                       EdgeInsets.symmetric(horizontal: 8),
-                                  child: TextFormField(
-                                    minLines: 1,
-                                    controller: massagecontroler,
-                                    decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: Theme.of(context).appColors.white100,
-                                      hintText: "send massage",
-                                      labelStyle: Theme.of(context)
-                                          .appTexts
-                                          .labelMedium
-                                          .copyWith(
-                                            color: Theme.of(context)
-                                                .appColors
-                                                .black37,
-                                          ),
-                                      focusedBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(50),
-                                        borderSide: BorderSide(
-                                            color: Theme.of(context)
-                                                .appColors
-                                                .secondary,
-                                            width: 0.5),
-                                      ),
-                                      enabledBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(50),
-                                        borderSide: BorderSide(
-                                            color: Theme.of(context)
-                                                .appColors
-                                                .secondary,
-                                            width: 0.5),
-                                      ),
-                                      contentPadding:
-                                          const EdgeInsets.symmetric(
-                                              horizontal: 16, vertical: 6),
-                                    ),
-                                  ),),),
-                          Expanded(
-                            flex: 2,
-                            child: IconButton(
-                                onPressed: _sendMessage,
-                                icon: SvgPicture.asset(Assests.sendicon)),
-                          )
-                        ],
-                      )),
-                )
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        if (userMessage != null)
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 8, horizontal: 12),
+                              margin: const EdgeInsets.only(
+                                  top: 4, bottom: 4, left: 50, right: 8),
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).appColors.primary,
+                                borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(16),
+                                    topRight: Radius.circular(16),
+                                    bottomLeft: Radius.circular(16)),
+                              ),
+                              child: Text(
+                                userMessage,
+                                style: TextStyle(
+                                    color:
+                                        Theme.of(context).appColors.white100),
+                              ),
+                            ),
+                          ),
+                        if (botMessage != null)
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 8, horizontal: 12),
+                              margin: const EdgeInsets.only(
+                                  top: 4, bottom: 4, left: 8, right: 50),
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).appColors.black16,
+                                borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(16),
+                                    topRight: Radius.circular(16),
+                                    bottomRight: Radius.circular(16)),
+                              ),
+                              child: Text(
+                                botMessage,
+                                style: TextStyle(
+                                    color: Theme.of(context).appColors.black87),
+                              ),
+                            ),
+                          ),
+                        const SizedBox(height: 4),
+                      ],
+                    );
+                  },
+                ),
               ],
-            ));
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: SizedBox(
+              height: 50,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 12,
+                    child: Container(
+                      height: 38,
+                      width: double.maxFinite,
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: TextFormField(
+                        minLines: 1,
+                        controller: massagecontroler,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Theme.of(context).appColors.white100,
+                          hintText: "send massage",
+                          labelStyle: Theme.of(context)
+                              .appTexts
+                              .labelMedium
+                              .copyWith(
+                                  color: Theme.of(context).appColors.black37),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                            borderSide: BorderSide(
+                                color: Theme.of(context).appColors.secondary,
+                                width: 0.5),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                            borderSide: BorderSide(
+                                color: Theme.of(context).appColors.secondary,
+                                width: 0.5),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 6),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: IconButton(
+                        onPressed: _sendMessage,
+                        icon: SvgPicture.asset(Assets.sendicon)),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/features/authentication/presentation/login/presentation/cubit/login_cubit.dart
+++ lib/features/authentication/presentation/login/presentation/cubit/login_cubit.dart
@@ -35,11 +35,11 @@ class LoginCubit extends BaseCubit{
   }
 
   Future<void> sendFcmToken() async {
-    final fcmToken = await _messaging.getToken();
-    print('Token: $fcmToken');
-    if (fcmToken != null) {
-      getIt<AuthenticationRepository>().sendFcmToken(fcmToken: fcmToken);
-    }
+   // final fcmToken = await _messaging.getToken();
+   // print('Token: $fcmToken');
+   // if (fcmToken != null) {
+    //  getIt<AuthenticationRepository>().sendFcmToken(fcmToken: fcmToken);
+    //}
   }
 
 }
