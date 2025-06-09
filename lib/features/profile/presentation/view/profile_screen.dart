7b311a5ae95b9e297d6bb4de1f36f88b48a79b94
Youssef Ahmed <163328420+Youssef-Ahmed238999@users.noreply.github.com>
2025-06-10 00:46:46 +0300

    refactor ui of profile header section

--- lib/features/profile/presentation/view/profile_screen.dart
+++ lib/features/profile/presentation/view/profile_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:planitly/design_system/theme.dart';
 import 'package:planitly/features/authentication/presentation/register/presentation/widgets/date_text_field.dart';
 import 'package:planitly/features/authentication/presentation/register/presentation/widgets/phone_text_field.dart';
@@ -22,25 +25,31 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  final TextEditingController fristNameController = TextEditingController();
-  final TextEditingController lastNameController = TextEditingController();
-  final TextEditingController phoneNumberController = TextEditingController();
-  final TextEditingController birthdayDateController = TextEditingController();
-  final TextEditingController oldpassController = TextEditingController();
-  final TextEditingController newpassController = TextEditingController();
-  final TextEditingController cnewpassController = TextEditingController();
+  final TextEditingController _fristNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _birthdayDateController = TextEditingController();
+  final TextEditingController _oldpassController = TextEditingController();
+  final TextEditingController _newpassController = TextEditingController();
+  final TextEditingController _cnewpassController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final ImagePicker _picker = ImagePicker();
+  File? _profileImage;
   String fristname = 'Menna';
   String lastname = 'Akram';
+  String username = "mennaakram12";
   String BirthdayDate = '1/1/1976';
   String phonenumber = '01244539870';
 
   @override
   void dispose() {
-    fristNameController.dispose();
-    lastNameController.dispose();
-    birthdayDateController.dispose();
-    phoneNumberController.dispose();
+    _fristNameController.dispose();
+    _lastNameController.dispose();
+    _birthdayDateController.dispose();
+    _phoneNumberController.dispose();
+    _oldpassController.dispose();
+    _newpassController.dispose();
+    _cnewpassController.dispose();
     super.dispose();
   }
 
@@ -48,98 +57,146 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).appColors.background,
-      appBar: CustomAppBar(
-        title: AppLocalizations.current.profile,
-        postfixIcon: Assets.edit,
-        onPostfixIconPressed: _openEditDialog,
-      ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 4),
-            height: 80,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(image: AssetImage(Assets.profile))),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 1),
-            height: 20,
-            child: Text(
-              "$fristname $lastname",
-              style: Theme.of(context)
-                  .appTexts
-                  .titleSmall
-                  .copyWith(color: Theme.of(context).appColors.black87),
+      appBar: _buildProfileAppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildProfileHeader(),
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 1),
-            height: 15,
-            child: Text(
-              "mennaakram12",
-              style: Theme.of(context)
-                  .appTexts
-                  .bodySmall
-                  .copyWith(color: Theme.of(context).appColors.black60),
+            SvgPicture.asset(Assets.line1),
+            SizedBox(
+              height: 29,
             ),
-          ),
-          SizedBox(
-            height: 9,
-          ),
-          SvgPicture.asset(Assets.line1),
-          SizedBox(
-            height: 29,
-          ),
-          ContactItem(
-            iconPath: Assets.phone,
-            title: 'Phone Number',
-            value: phonenumber,
-          ),
-          SizedBox(height: 20),
-          ContactItem(
-            iconPath: Assets.email,
-            title: 'Email',
-            value: 'menna@gmail.com',
-          ),
-          SizedBox(height: 20),
-          ContactItem(
-            iconPath: Assets.birthday,
-            title: 'Birthday',
-            value: BirthdayDate,
-          ),
-          SizedBox(height: 30),
-          SvgPicture.asset(Assets.line2),
-          SizedBox(height: 20),
-          Button(
-            text: "My Pages",
-            onTap: () {},
-          ),
-          Button(
-            text: "Change Password",
-            onTap: () => showchangePopup(context, () {}, oldpassController,
-                newpassController, cnewpassController),
-          ),
-          Button(
-            text: "Settings",
-            onTap: () {},
-          ),
-          Button(
-            text: "Logout",
-            textColor: Theme.of(context).appColors.red,
-            onTap: () => showlogoutPopup(context, () {}),
-          ),
-        ],
+            ContactItem(
+              iconPath: Assets.phone,
+              title: 'Phone Number',
+              value: phonenumber,
+            ),
+            SizedBox(height: 20),
+            ContactItem(
+              iconPath: Assets.email,
+              title: 'Email',
+              value: 'menna@gmail.com',
+            ),
+            SizedBox(height: 20),
+            ContactItem(
+              iconPath: Assets.birthday,
+              title: 'Birthday',
+              value: BirthdayDate,
+            ),
+            SizedBox(height: 30),
+            SvgPicture.asset(Assets.line2),
+            SizedBox(height: 20),
+            Button(
+              text: "My Pages",
+              onTap: () {},
+            ),
+            Button(
+              text: "Change Password",
+              onTap: () => showchangePopup(context, () {}, _oldpassController,
+                  _newpassController, _cnewpassController),
+            ),
+            Button(
+              text: "Settings",
+              onTap: () {},
+            ),
+            Button(
+              text: "Logout",
+              textColor: Theme.of(context).appColors.red,
+              onTap: () => showlogoutPopup(context, () {}),
+            ),
+          ],
+        ),
       ),
     );
   }
 
+  CustomAppBar _buildProfileAppBar() {
+    return CustomAppBar(
+      title: AppLocalizations.current.profile,
+      postfixIcon: Assets.edit,
+      onPostfixIconPressed: _openEditDialog,
+    );
+  }
+
+  Column _buildProfileHeader() {
+    return Column(
+      children: [
+        InkWell(
+          borderRadius: BorderRadius.circular(60),
+          onTap: _pickImage,
+          child: Container(
+            height: 80,
+            width: 80,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: _profileImage != null
+                        ? FileImage(_profileImage!) as ImageProvider
+                        : AssetImage(Assets.profile))),
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(
+          "$fristname $lastname",
+          style: Theme.of(context)
+              .appTexts
+              .titleSmall
+              .copyWith(color: Theme.of(context).appColors.black87),
+        ),
+        Text(
+          username,
+          style: Theme.of(context)
+              .appTexts
+              .bodySmall
+              .copyWith(color: Theme.of(context).appColors.black60),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _pickImage() async {
+    final toolbarColor = Theme.of(context).appColors.primary;
+    final toolbarWidgetColor = Theme.of(context).appColors.white100;
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (image != null) {
+      final croppedFile = await ImageCropper().cropImage(
+        sourcePath: image.path,
+        aspectRatio: const CropAspectRatio(ratioX: 1, ratioY: 1),
+        uiSettings: [
+          AndroidUiSettings(
+            toolbarTitle: 'Crop Image',
+            toolbarColor: toolbarColor,
+            toolbarWidgetColor: toolbarWidgetColor,
+            initAspectRatio: CropAspectRatioPreset.square,
+            lockAspectRatio: true,
+          ),
+          IOSUiSettings(
+            title: 'Crop Image',
+          ),
+        ],
+      );
+
+      if (croppedFile != null) {
+        setState(() {
+          _profileImage = File(croppedFile.path);
+        });
+      }
+    }
+  }
+
   void _openEditDialog() {
-    fristNameController.text = fristname;
-    lastNameController.text = lastname;
-    phoneNumberController.text = phonenumber;
-    birthdayDateController.text = BirthdayDate;
+    _fristNameController.text = fristname;
+    _lastNameController.text = lastname;
+    _phoneNumberController.text = phonenumber;
+    _birthdayDateController.text = BirthdayDate;
 
     context.alertDialog(
       AppLocalizations.current.editInfo,
@@ -161,7 +218,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 Expanded(
                   child: CustomTextField(
                     labelText: AppLocalizations.current.firstName,
-                    controller: fristNameController,
+                    controller: _fristNameController,
                     keyboardType: TextInputType.name,
                     validator: Validators.nameValidator,
                   ),
@@ -170,7 +227,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 Expanded(
                   child: CustomTextField(
                     labelText: AppLocalizations.current.lastName,
-                    controller: lastNameController,
+                    controller: _lastNameController,
                     keyboardType: TextInputType.name,
                     validator: Validators.nameValidator,
                   ),
@@ -180,13 +237,13 @@ class _ProfileScreenState extends State<ProfileScreen> {
             const SizedBox(height: 16),
             PhoneNumberTextField(
               labelText: AppLocalizations.current.phoneNumber,
-              controller: phoneNumberController,
+              controller: _phoneNumberController,
               validator: Validators.phoneNumberValidator,
             ),
             const SizedBox(height: 16),
             DateTextField(
               labelText: AppLocalizations.current.birthdayDate,
-              controller: birthdayDateController,
+              controller: _birthdayDateController,
               validator: Validators.cantBeEmpty,
             ),
           ],
